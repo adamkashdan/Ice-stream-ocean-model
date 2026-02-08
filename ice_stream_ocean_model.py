15a3420793ad6200be8f7baef162079056a70fbb
Adam Kashdan <adamkashtanov@gmail.com>
2026-02-08 07:04:11 -0500

    Update author information in ice_stream_ocean_model.py

--- ice_stream_ocean_model.py
+++ ice_stream_ocean_model.py
@@ -1,6 +1,6 @@
 """
 Directional Coupled Ice Stream Ocean Model
-Logan E. Mann // Ice and Climate Group Georgia Tech
+Alex Robel & Logan E. Mann // Ice and Climate Group Georgia Tech
 Converted from MATLAB to Python
 """
 
