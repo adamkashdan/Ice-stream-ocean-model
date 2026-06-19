9e6826e4489b08b980e7716b93edcc41b8e2de24
Adam Kashdan <adamkashtanov@gmail.com>
2026-06-19 19:10:04 -0400

    Update citation and description in the simulation file

--- zoet_stick_slip_cycles_storfjordenGWZs.py
+++ zoet_stick_slip_cycles_storfjordenGWZs.py
@@ -1,8 +1,6 @@
 """
 GZW Formation Simulation — calibrated to R_2018 seismic profile
-Rasmussen Nielsen Storfjorden GWZs 2018
-"Reconstruction of ice sheet retreat after the Last Glacial Maximum in
-Storfjorden, southern Svalbard"
+Nielsen, T., & Rasmussen, T. L. (2018). Reconstruction of ice sheet retreat after the Last Glacial maximum in Storfjorden, southern Svalbard. Marine Geology, 402, 228-243.
 Fig. 6. Seismic lines showing cross-sections parallel (N-S) and perpendicular (E-W) to the ice flow.
 a–b) Inner GZW; c–d) Middle GZW; e–f) Outer GZW
 ================================================================
